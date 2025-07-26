93a667183f653aa1d5cc3352cb7193ed312f3af5
harsht1608 <91127719+harsht1608@users.noreply.github.com>
2025-07-26 18:00:55 +0530

    updated script file

--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Start Main server
+echo "Starting Main_server..."
 cd Main_server && npm install &
 PID1=$!
 wait $PID1
 node server.js &
 
 # Start secondary server
+echo "Starting src server..."
 cd ../src && npm install &
 PID2=$!
 wait $PID2
 npm start &
 
+echo "Both servers started."
 # Keep script running
 wait
